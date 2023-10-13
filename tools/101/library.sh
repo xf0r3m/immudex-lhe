dfe4ece6d71c85fee65f30559e4b91403d9efd71
xf0r3m <jakubstasinski@protonmail.com>
2023-10-13 18:53:35 +0200

    Przygotowania do wydania immudex-lhe w wersji 0.0.1

---
+++ tools/101/library.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+function get_debian_branch() {
+  if grep -q 'trixie' /etc/os-release; then
+    echo "testing";
+  else
+    echo "stable";
+  fi
+}
+
+function get_machine_arch() {
+  arch=$(uname -m);
+  if [ "$arch" = "i686" ]; then
+    echo "32";
+  else
+    echo "64";
+  fi
+}
+
+function check_distro_version() {
+  set -e
+  root="/run/live/medium";
+  if [ -d ${root}/live ]; then
+    if [ -f ${root}/live/version ]; then 
+      version=$(cat ${root}/live/version | sed 's/\.//g');
+    else version="000";
+    fi
+    BRANCH=$(get_debian_branch);
+    ARCH=$(get_machine_arch);
+    if [ ! -f /tmp/ltver ]; then
+      wget -q https://ftp.morketsmerke.org/immudex/${BRANCH}/upgrades/latest/${ARCH}/version -O /tmp/ltver;
+    fi
+	  if [ ! -s /tmp/ltver ]; then sudo rm /tmp/ltver; return 255; fi;
+    newVersionTxt=$(cat /tmp/ltver);
+    newVersionInt=$(echo $newVersionTxt | sed 's/\.//g');
+    if [ $version -lt $newVersionInt ]; then
+     exitcode=0;
+    else
+     exitcode=1;
+    fi
+  else
+    exitcode=255;
+  fi
+  if [ "$1" ] && [ "$1" = "--print" ]; then
+    echo $newVersionTxt;
+    return 0;
+  fi
+  return $exitcode;
+}
+
+function ascii_colors() {
+
+  BLUE="\e[1;94m";
+  RED="\e[1;91m";
+  CYAN="\e[1;96m";
+  ENDCOLOR="\e[0m";
+
+  echo -e "${BLUE} _                           ${RED}    _      ${CYAN}      ${ENDCOLOR}";
+  echo -e "${BLUE}(_)_ __ ___  _ __ ___  _   _ ${RED} __| | ___${CYAN}__  __${ENDCOLOR}";
+  echo -e "${BLUE}| | '_ \` _ \| '_ \` _ \| | | |${RED}/ _\` |/ _ \\\\${CYAN} \/ /${ENDCOLOR}";
+  echo -e "${BLUE}| | | | | | | | | | | | |_| |${RED} (_| |  __/${CYAN}>  < ${ENDCOLOR}";
+  echo -e "${BLUE}|_|_| |_| |_|_| |_| |_|\__,_|${RED}\__,_|\___/${CYAN}_/\_\\";
+  echo -e "${ENDCOLOR}";
+
+}
---
+++ versions/001.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+export VERSION=$(echo $0 | cut -d "." -f 1);
+
+dhclient;
+cd
+if [ -x /usr/bin/git ]; then git clone https://github.com/xf0r3m/immudex-lhe;
+else apt install git && git clone https://github.com/xf0r3m/immudex-lhe;
+fi
+source ~/immudex-lhe/versions/template.sh;
+
+update_packages;
+
+wget https://github.com/yt-dlp/yt-dlp/releases/download/2023.10.07/yt-dlp -O /usr/bin/youtube-dl;
+
+install_packages firejail;
+cp -vv ~/immudex/files/${VERSION}/firejail.config /etc/firejail;
+
+wget https://ftp.morketsmerke.org/immudex/testing/software/librewolf/librewolf-118.0.1-1.en-US.linux-i686.tar.bz2;
+tar -xf librewolf-118.0.1-1.en-US.linux-i686.tar.bz2 -C /usr/lib;
+rm librewolf-118.0.1-1.en-US.linux-i686.tar.bz2;
+ln -s /usr/lib/librewolf/librewolf /usr/bin/librewolf;
+update-alternatives --remove icecat /usr/bin/icecat;
+update-alternatives --install /usr/bin/x-www-browser librewolf /usr/bin/librewolf 100;
+rm -v /usr/bin/icecat;
+rm -rfv /usr/lib/icecat;
+tar -xf ~/immudex-lhe/files/${VERSION}/librewolf.tgz -C /etc/skel;
+
+
+cp -vv ~/immudex-lhe/${VERSION}/idle-clic /usr/local/bin;
+cp -vv ~/immudex-lhe/${VERSION}/pl /usr/local/bin;
+cp -vv ~/immudex-lhe/${VERSION}/secure-firefox /usr/local/bin;
+cp -vv ~/immudex-lhe/${VERSION}/library.sh /usr/local/bin;
+cp -vv ~/immudex-lhe/${VERSION}/motd2 /usr/local/bin;
+cp -vv ~/immudex-lhe/${VERSION}/newsfeed /usr/local/bin;
+cp -vv ~/immudex-lhe/${VERSION}/sync.sh /usr/local/bin;
+cp -vv ~/immudex-lhe/${VERSION}/unlock-ds /usr/local/bin;
+
+chmod +x /usr/local/bin/*;
+
+cp -rvv ~/immudex-lhe/${VERSION}/files/sync.sh /usr/share/;
+
+recreate_user;
+
+tidy;
+
